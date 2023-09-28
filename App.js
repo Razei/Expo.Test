1e588525b83784ff51e614e5bf54aeab112a5b75
Jarod <razeixyz@gmail.com>
2023-09-27 22:36:11 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}> 
+      <View style={styles.container}>
+        <Text>Current Weather</Text>
+      </View>
+    </SafeAreaView>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  wrapper: {
+    flex: 1
+  }
+});
+
+export default App;
