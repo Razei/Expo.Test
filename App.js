ed97b4994283bbd0c35ce92f951b4f92e45b0837
Jarod <razeixyz@gmail.com>
2023-09-28 20:20:56 -0400

    Components and flatlist styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useFonts } from 'expo-fonts';
+import { useFonts } from 'expo-font';
 
 // https://www.youtube.com/watch?v=_ivIUCSOZ78
 
@@ -21,7 +21,7 @@ const App = () => {
   const [loaded] = useFonts({
     InterBold: require('./assets/fonts/Inter-Bold.ttf'),
     InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
-    InterMedium: require('./assets/fonts/Inter-Mediun.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
     InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
     InterLight: require('./assets/fonts/Inter-Light.ttf'),
   });
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,45 @@
-import { Text } from 'react-native';
-
+import { SafeAreaView, Text, StyleSheet, View, FlatList } from "react-native";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
   return (
-    <Text>Home</Text>
-  )
-}
+    <SafeAreaView style={style.container}>
+      <FocusedStatusBar background={COLORS.primary} />
 
-export default Home
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          ></FlatList>
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Home;
